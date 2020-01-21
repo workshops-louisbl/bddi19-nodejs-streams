0e7dc497870513b6f38b0012bf14cb88a5654a9d
louisbl <louis@beltramo.me>
2020-01-21 17:43:23 +0100

    add twitter stream transforms

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ require("dotenv").config();
 const { server } = require("./lib/server");
 const twitterStream = require("./lib/twitter");
 
+twitterStream.on("error", error => {
+  console.error(error);
+})
+
 server.listen(process.env.PORT);
