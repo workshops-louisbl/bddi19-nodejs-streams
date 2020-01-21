f0930216edc8c93bbeb98bdb39630efe60125dbf
louisbl <louis@beltramo.me>
2020-01-21 18:26:38 +0100

    connect client with websocket

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 
-const { server } = require("./lib/server");
+const { wsServer, server } = require("./lib/server");
 const twitterStream = require("./lib/twitter");
 
 twitterStream.on("error", error => {
   console.error(error);
+})
+
+wsServer.on("connection", client => {
+  console.log("new client connection");
+
+  client.on("message", message => {
+    console.log("message from client: ", message);
+  })
+
+  client.send("Welcome!");
 })
 
 server.listen(process.env.PORT);
